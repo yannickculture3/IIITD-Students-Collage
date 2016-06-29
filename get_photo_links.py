82340036f25ef4084971e0fbed4a8aa3a8739233
yannickculture3 <arisetrefles@gmail.com>
2016-06-29 21:07:25 +0400

    Update get_photo_links.py

--- get_photo_links.py
+++ get_photo_links.py
@@ -1,19 +1,135 @@
-import json
-from urllib2 import urlopen
-
-
-fp = open("iiitd_group_members.txt")
-data = json.loads(fp.read())
-fp.close()
-
-user_photos = {} # id -> [User's Name, Photo URL]
-
-for user in data["data"]:
-    print user
-    page = urlopen("http://graph.facebook.com/" + user["id"] + "?fields=picture")
-    page_data = json.loads(page.read())
-    photo_url = page_data["picture"]["data"]["url"]
-    user_photos[user["id"]] = [user["name"], photo_url]
-
-fp = open("user_photos.json", "w")
-fp.write(json.dumps(user_photos))
+{
+  "members": {
+    "data": [
+      {
+        "name": "Lucas Riscado",
+        "administrator": false,
+        "id": "1505977076334645"
+      },
+      {
+        "name": "Ant Hony",
+        "administrator": false,
+        "id": "122918001420870"
+      },
+      {
+        "name": "Audrey Fieulaine",
+        "administrator": false,
+        "id": "1451312468520392"
+      },
+      {
+        "name": "Theo Mayoss",
+        "administrator": false,
+        "id": "634697636613899"
+      },
+      {
+        "name": "Sammy Zepekenio",
+        "administrator": false,
+        "id": "693383370730030"
+      },
+      {
+        "name": "Sarah Duvalier",
+        "administrator": false,
+        "id": "132383830521418"
+      },
+      {
+        "name": "Momo Tarsitano",
+        "administrator": false,
+        "id": "267005706989728"
+      },
+      {
+        "name": "Soso Bambino",
+        "administrator": false,
+        "id": "1209965062389572"
+      },
+      {
+        "name": "Big-ben Junior Bigben",
+        "administrator": false,
+        "id": "407860422752009"
+      },
+      {
+        "name": "Jimmy Twotime",
+        "administrator": false,
+        "id": "10204099341826022"
+      },
+      {
+        "name": "Tiavina Alias Kakashi",
+        "administrator": false,
+        "id": "743163092432585"
+      },
+      {
+        "name": "Anas Ouissam",
+        "administrator": false,
+        "id": "1043349982374212"
+      },
+      {
+        "name": "Tuma Reconnu",
+        "administrator": false,
+        "id": "159084084504923"
+      },
+      {
+        "name": "Brahm's Algero'cain",
+        "administrator": false,
+        "id": "492474614260142"
+      },
+      {
+        "name": "Diego Milanello",
+        "administrator": false,
+        "id": "470423459756634"
+      },
+      {
+        "name": "Anis Lbh",
+        "administrator": false,
+        "id": "854257068015661"
+      },
+      {
+        "name": "Mehdi Zyati",
+        "administrator": false,
+        "id": "835177416576242"
+      },
+      {
+        "name": "Vincent Magnan",
+        "administrator": false,
+        "id": "694026317356929"
+      },
+      {
+        "name": "Rayane de Bokr",
+        "administrator": false,
+        "id": "1028378677222105"
+      },
+      {
+        "name": "Soumsoum Cba",
+        "administrator": false,
+        "id": "936802613045200"
+      },
+      {
+        "name": "Theophile Mzd",
+        "administrator": false,
+        "id": "1586854561586100"
+      },
+      {
+        "name": "Inconito Elb",
+        "administrator": false,
+        "id": "102792886789446"
+      },
+      {
+        "name": "Alexis Balland",
+        "administrator": false,
+        "id": "323800557807121"
+      },
+      {
+        "name": "Siidiik Levéridiik",
+        "administrator": false,
+        "id": "757626850944544"
+      },
+      {
+        "name": "Dylan Wagner",
+        "administrator": false,
+        "id": "896003280432753"
+      }
+    ],
+    "paging": {
+      "next": "https://graph.facebook.com/v2.6/124790354528303/members?access_token=EAACEdEose0cBAHeZAM9leiZCniaFgZBzjA5yB7MrRKWmfW2LJiipQkK99UP2ZCHwfCIF55g2XxBJDwHQxkCozzYVsDRSNtJ5oD13bwBTiGbARUovL4tZAL4p6zusatm7qNff2LIZCje0WNgpGpuhc5qIJvu3tJWFUxOWdTKhhf6QZDZD&limit=25&offset=25&__after_id=enc_AdD7whOacjZBVTFZAApBB9ILeQiYuzuIw1hJi6Su3x4lE3LzbxvlarioCdZBMPCeYZBVgVasqNnADDHvELsOZCgCuPLdj"
+    }
+  },
+  "id": "124790354528303"
+}
